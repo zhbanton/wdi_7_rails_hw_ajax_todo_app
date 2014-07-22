1679d5b7beb8826fab89784c6331ec63bc3e62ca
Alex Grant <grantovich@users.noreply.github.com>
2014-07-22 15:42:43 -0400

    Add js-routes configuration

---
+++ config/initializers/jsroutes.rb
@@ -0,0 +1,5 @@
+JsRoutes.setup do |config|
+  # Default value, change to `true` if you want camelCase route helpers
+  #   e.g. `Routes.newUserPath` instead of `Routes.new_user_path`
+  config.camel_case = false
+end
